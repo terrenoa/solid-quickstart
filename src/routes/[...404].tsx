92c4c280aa4b37678ba1d758a250515241590afa
terrenoa <85424039+terrenoa@users.noreply.github.com>
2023-11-11 17:23:48 -0300

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -6,13 +6,13 @@ export default function NotFound() {
     <main>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
+      <h1>Alejo N. Terreno</h1>
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
+        Visita{" "}
+        <a href="https://www.linkedin.com/in/terrenoa/" target="_blank">
+          mi LinkedIn
         </a>{" "}
-        to learn how to build SolidStart apps.
+        para conocerme un poco más.
       </p>
     </main>
   );
